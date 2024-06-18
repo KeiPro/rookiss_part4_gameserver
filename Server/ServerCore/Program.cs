da16b113fe0cd5e38d56196f4f10f1b32737a886
Kim youngwoo <keiprogramo@gmail.com>
2024-06-19 01:10:18 +0900

    [컴파일러 최적화] 전역변수에 대한 디버그 모드와 릴리즈 모드의 차이점

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -2,35 +2,35 @@
 {
     internal class Program
     {
-        static void MainThread(object state)
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for (int i = 0; i < 5; i++)
-                Console.WriteLine("Hello Thread!");
+            Console.WriteLine("쓰레드 시작!");
+
+            while (_stop == false)
+            { 
+                // 누군가가 stop 신호를 해주기를 기다린다.
+            }
+
+            Console.WriteLine("쓰레드 종료!");
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1); // 최소 1개
-            ThreadPool.SetMaxThreads(5, 5); // 최대 5개
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            for (int i = 0; i < 5; i++)
-            {
-                // LongRunning옵션을 사용하면 -> 워커 쓰레드에서 관리가 되지 않도록 하는 처리가 된다.
-                // 해당 옵션을 사용하지 않았다면 워커 쓰레드에서 관리가 되기 때문에 
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning); 
-                t.Start();
-            }
+            Thread.Sleep(1000);
 
-            ThreadPool.QueueUserWorkItem(MainThread);
+            _stop = true;
 
-            //Thread t = new Thread(MainThread); 
-            //t.Name = "Test Thread";
-            //t.IsBackground = true; // 백그라운드에서 실행한다. Main함수가 끝이나면 쓰레드도 종료가 된다.
-            //t.Start();
-            //Console.WriteLine("Wating for Thread!");
-            //t.Join(); // 쓰레드가 종료가 되면 넘어간다. 즉, 쓰레드가 실행되는 동안에는 계속 유지된다.
-            //Console.WriteLine("Hello World!");
-            while (true) ;
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+
+            t.Wait(); // thread에서는 join. task에서는 Wait.
+
+            Console.WriteLine("종료 성공");
         }
     }
 }
