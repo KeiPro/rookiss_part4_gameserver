0233df5c1ec4f5c462d53068c47f3a05cf40b348
Kim youngwoo <keiprogramo@gmail.com>
2024-06-20 23:44:38 +0900

    [Interlocked] 경쟁 조건에 대한 해결책

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,55 +1,35 @@
 ﻿namespace ServerCore
 {
-    // 메모리 배리어
-    // A) 코드 재배치 억제
-    // B) 가시성
-
-    // 1) Full Memory Barrier (ASM MFENCE, C# Thread.MemoryBarrier) : Store/Load 둘 다 막는다.
-    // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다
-    // 3) Load Memory Barrier (ASM LFENCE) : Load만 막는다
-
     internal class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1; // Store y
-
-            Thread.MemoryBarrier();
-
-            r1 = x; // Load x
+            for (int i = 0; i < 1000000; i++)
+            {
+                int afterValue = Interlocked.Increment(ref number);
+            }
         }
 
         static void Thread_2()
         {
-            x = 1; // Store x
-
-            Thread.MemoryBarrier();
-
-            r2 = y; // Load y
+            for (int i = 0; i < 1000000; i++)
+            {
+                Interlocked.Decrement(ref number);
+            }
         }
 
         static void Main(string[] args)
         {
-            while(true)
-            {
-                x = y = r1 = r2 = 0;
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
 
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
+            t1.Start();
+            t2.Start();
 
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2); // t1, t2 쓰레드가 끝날때까지 기다림.
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(number);
         }
     }
 }
