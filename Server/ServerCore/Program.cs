a6f565278ad009ed27a6b0a4ee009bf28e2a8e29
Kim youngwoo <keiprogramo@gmail.com>
2024-06-28 17:39:46 +0900

    [Context Swiching] 쓰레드 양보하기

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -15,6 +15,11 @@
                 // locked와 expected의 값을 비교해서 같으면 _locked의 값을 desired값으로 바꿔준다.
                 if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
                     break;
+
+
+                // Thread.Sleep(1); // 무조건 휴식 => 무조건 1ms 정도 쉬겠다는 뜻.
+                // Thread.Sleep(0); // 조건부 양보 => 나보다 우선순위가 낮은 얘들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테 돌아옴.
+                Thread.Yield(); // 관대한 양보 => 관대하게 양보할테니, 지금 실행이 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 얘가 없으면 남은 시간을 보인한테 소진.
             }
         }
 
