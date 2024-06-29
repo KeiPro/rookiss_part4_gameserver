41e8d6c08c9a44b61298e4e88587c298b18ce1eb
Kim youngwoo <keiprogramo@gmail.com>
2024-06-30 02:54:17 +0900

    [ReaderWriterLock 구현 연습] WriteLock, WriteUnlock 구현

---
+++ Server/ServerCore/Lock.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    // Lock을 만들 때에는 정책을 정해야 한다.
+    // 재귀적 락을 허용할지? (No)
+    // 스핀락 정책 (5000번 -> Yield)
+    internal class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+
+        // [Unused(1)] [WriteThreadId(15)] [ReadCount(16)]
+        int _flag = EMPTY_FLAG;
+
+        public void WriteLock()
+        {
+            // 아무도 WriteLock 또는 ReadLock을 획득하고 있지 않을 때, 경합해서 소유권을 얻는다
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    // 시도를 해서 성공하면 쓰레드 아이디를 플래그에 넣어준다.
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                        return;
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock()
+        { 
+        
+        }
+
+        public void ReadUnlock()
+        { 
+        
+        }
+    }
+}
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,61 +1,10 @@
 ﻿namespace ServerCore
 {
-    class Lock
-    {
-        // AutoResetEvent : 커널단의 관리자가 관리하고 있는 bool변수.
-        // param : true는 문을 연 상태로 시작할 것인가? 닫은 상태로 시작할 것인가?에 대한 bool값임.
-        // Auto라는 단어의 뜻은 하나가 지나가면 자동으로 문을 닫아준다는 클래스가 된다.
-        ManualResetEvent _available = new ManualResetEvent(true);
-
-        public void Acquire()
-        {
-            // 입장 시도 -> _available의 bool변수가 false로 바뀐다. _available.Reset()함수가 포함되어 있음.
-            _available.WaitOne();
-
-            // _available.Reset(); // bool = false
-        }
-
-        public void Release()
-        {
-            _available.Set(); // 원상 복귀를 하는 코드 즉, 맨 처음에 true로 설정했으므로 true로 돌려준다.
-        }
-    }
-
     internal class Program
     {
-        static int _num = 0;
-        static Lock _lock = new Lock();
-
-        static void Thread_1()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.Acquire();
-                _num++;
-                _lock.Release();
-            }
-        }
-
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.Acquire();
-                _num--;
-                _lock.Release();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-            Console.WriteLine(_num);
+           
         }
     }
 }
