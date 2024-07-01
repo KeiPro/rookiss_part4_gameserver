03d8a1f16f7deb02c1ee49b24b823a85be498d3e
Kim youngwoo <keiprogramo@gmail.com>
2024-07-01 18:38:24 +0900

    [Listener] 코드 분리(Listner 클래스 생성) / 블록킹 계열 함수를 논블록킹 계열함수로 수정하는 작업 진행(비동기 코드)

---
+++ Server/ServerCore/Listener.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerCore
+{
+    internal class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            // 문지기 교육
+            _listenSocket.Bind(endPoint);
+
+            // 영업 시작
+            // backlog : 최대 대기수
+            _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            // args에 대해서 초기화를 시켜주는 것이 중요하다.
+            // 아래의 OnAcceptCompleted에서 args를 넘겨주고 있는데 이 args는 null이 아닌 어떤 데이터들이 들어가있는 형태이기 때문에..
+            args.AcceptSocket = null;
+
+            bool pending = _listenSocket.AcceptAsync(args);
+
+            // AcceptAsync를 호출했을 때,
+            // 정말 운 좋게 동시에 클라이언트의 접속 요청이 pending없이 오게 된다면 OnAcceptCompleted()함수 호출.
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                // TODO
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            // 여기까지 왔다는 것은 정상적으로 성공했다는 뜻이니,
+            // 다음 소켓을 위해서 또 한 번 등록을 해주기 위해 호출하는 것.
+            RegisterAccept(args);
+        }
+    }
+}
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,6 +6,32 @@ namespace ServerCore
 {
     internal class Program
     {
+        static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                // 받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+                clientSocket.Send(sendBuff);
+
+                // 쫓아낸다
+                clientSocket.Shutdown(SocketShutdown.Both); // 듣기도 싫고, 말하기도 싫다를 명시해주기.
+                clientSocket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
         static void Main(string[] args)
         {
             // DNS (Domain Name System)
@@ -15,43 +41,13 @@ namespace ServerCore
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // 문지기
-            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _listener.Init(endPoint, OnAcceptHandler);
+            Console.WriteLine("Listening...");
 
-            try
+            // 코드 종료만 안되게 무한루프를 돌림.
+            while (true)
             {
-                // 문지기 교육
-                listenSocket.Bind(endPoint);
-
-                // 영업 시작
-                // backlog : 최대 대기수
-                listenSocket.Listen(10);
-
-                while (true)
-                {
-                    Console.WriteLine("Listening...");
-
-                    // 손님을 입장시킨다
-                    Socket clientSocket = listenSocket.Accept(); // 세션의 소켓
-
-                    // 받는다
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = clientSocket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Client] {recvData}");
-
-                    // 보낸다
-                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
-                    clientSocket.Send(sendBuff);
-
-                    // 쫓아낸다
-                    clientSocket.Shutdown(SocketShutdown.Both); // 듣기도 싫고, 말하기도 싫다를 명시해주기.
-                    clientSocket.Close();
-                }
-            } 
-            catch (Exception ex) 
-            {
-                Console.WriteLine(ex.ToString());
+                ;
             }
         }
     }
