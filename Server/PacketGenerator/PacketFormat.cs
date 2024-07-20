1559a57bbbfc89e2b603c51de8e3a577d594d069
Kim youngwoo <keiprogramo@gmail.com>
2024-07-20 18:10:08 +0900

    [Unity 연동2] PacketQueue라는 것을 이용하여 RecvPacket과 HandlePacket을 나누는 작업 진행 \n 유니티에서는 멀티 스레드를 지원하지 않기 때문에 제대로 동작되지 않는 현상이 있어, 서브 스레드에서 패킷을 받을 경우 PacketQueue에 저장만 시켜놓고 메인 스레드의 Update문에서 PacketQueue에 있는 것을 꺼내어 실행하기 위함.

--- Server/PacketGenerator/PacketFormat.cs
+++ Server/PacketGenerator/PacketFormat.cs
@@ -11,7 +11,10 @@ namespace PacketGenerator
 
         // {0} 패킷 등록
         public static string managerFormat =
-@"using ServerCore;
+@"using System;
+using System.Collections.Generic;
+using ServerCore;
+
 
 class PacketManager
 {{
@@ -25,7 +28,7 @@ class PacketManager
         Register();
     }}
 
-    Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>> _onRecv = new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>>();
+    Dictionary<ushort, Func<PacketSession, ArraySegment<byte>, IPacket>> _makeFunc = new Dictionary<ushort, Func<PacketSession, ArraySegment<byte>, IPacket>>();
     Dictionary<ushort, Action<PacketSession, IPacket>> _handler = new Dictionary<ushort, Action<PacketSession, IPacket>>();
 
     public void Register()
@@ -33,7 +36,7 @@ class PacketManager
 {0}
     }}
 
-    public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer)
+    public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer, Action<PacketSession, IPacket> onRecvCallback = null)
     {{
         ushort count = 0;
 
@@ -42,15 +45,26 @@ class PacketManager
         ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + count);
         count += 2;
 
-        Action<PacketSession, ArraySegment<byte>> action = null;
-        if (_onRecv.TryGetValue(id, out action))
-            action.Invoke(session, buffer);
+        Func<PacketSession, ArraySegment<byte>, IPacket> func = null;
+        if (_makeFunc.TryGetValue(id, out func))
+        {{
+            IPacket packet = func.Invoke(session, buffer);
+            if (onRecvCallback != null)
+                onRecvCallback.Invoke(session, packet);
+            else
+                HandlePacket(session, packet);
+        }}
     }}
 
-    void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IPacket, new()
+    T MakePacket<T>(PacketSession session, ArraySegment<byte> buffer) where T : IPacket, new()
     {{
         T packet = new T();
         packet.Read(buffer);
+        return packet;
+    }}
+
+    public void HandlePacket(PacketSession session, IPacket packet)
+    {{
         Action<PacketSession, IPacket> action = null;
         if (_handler.TryGetValue(packet.Protocol, out action))
             action.Invoke(session, packet);
@@ -59,7 +73,7 @@ class PacketManager
 
         // {0} 패킷 이름
         public static string managerRegisterFormat =
-@"        _onRecv.Add((ushort)PacketID.{0}, MakePacket<{0}>);
+@"      _makeFunc.Add((ushort)PacketID.{0}, MakePacket<{0}>);
         _handler.Add((ushort)PacketID.{0}, PacketHandler.{0}Handler);";
 
         // {0} 패킷 이름 / 번호 목록
@@ -76,7 +90,7 @@ public enum PacketID
     {0}
 }}
 
-interface IPacket
+public interface IPacket
 {{
 	ushort Protocol {{ get; }}
 	void Read(ArraySegment<byte> segment);
