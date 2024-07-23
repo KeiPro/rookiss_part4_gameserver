9022b691c14bdc6e2a86b1f17990a68c19b6a89a
Kim youngwoo <keiprogramo@gmail.com>
2024-07-23 16:35:41 +0900

    [유니티 연동3] 플레이어 접속 및 이동 패킷 추가

--- Server/Server/Packet/PacketHandler.cs
+++ Server/Server/Packet/PacketHandler.cs
@@ -9,9 +9,8 @@ using System.Threading.Tasks;
 
 internal class PacketHandler
 {
-    public static void C_ChatHandler(PacketSession session, IPacket packet)
+    public static void C_LeaveGameHandler(PacketSession session, IPacket packet)
     {
-        C_Chat chatPacket = packet as C_Chat;
         ClientSession clientSession = session as ClientSession;
 
         if (clientSession.Room == null)
@@ -19,7 +18,23 @@ internal class PacketHandler
 
         GameRoom room = clientSession.Room;
         room.Push(
-            () => room.Broadcast(clientSession, chatPacket.chat)
+            () => room.Leave(clientSession)
+        );
+    }
+
+    public static void C_MoveHandler(PacketSession session, IPacket packet)
+    {
+        C_Move movePacket = packet as C_Move;
+        ClientSession clientSession = session as ClientSession;
+
+        if (clientSession.Room == null)
+            return;
+
+        Console.WriteLine($"{movePacket.posX}, {movePacket.posY}, {movePacket.posZ}");
+
+        GameRoom room = clientSession.Room;
+        room.Push(
+            () => room.Move(clientSession, movePacket)
         );
     }
 }
